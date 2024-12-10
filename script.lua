3f40dfb28371b91c450e0fcd47facfd4a05e53e7
Adam Dev <114385858+adamdev20@users.noreply.github.com>
2024-12-10 21:08:23 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -15,7 +15,7 @@ local Window = Fluent:CreateWindow({
 --Fluent provides Lucide Icons https://lucide.dev/icons/ for the tabs, icons are optional
 local Tabs = {
     Main = Window:AddTab({ Title = "Main", Icon = "" }),
-    tp = Window:AddTab({ Title = "Teleport", Icon = "" }),
+    esp = Window:AddTab({ Title = "Esp", Icon = "" }),
     Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
 }
 
